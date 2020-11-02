bb03770e7abf6c5dae9485e755a913ff9f521baf
zach scharoun <zscharoun@gmail.com>
2020-11-02 14:19:05 -0500

    Adds appointment model and service

--- src/app/components/nurse-home/nurse-home.component.ts
+++ src/app/components/nurse-home/nurse-home.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Appointment } from 'src/app/models/appointment';
+import { AppointmentService } from 'src/app/services/appointment.service';
 
 @Component({
   selector: 'app-nurse-home',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NurseHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appointmentService:AppointmentService) { }
 
   ngOnInit(): void {
+    this.getAllAppointments()
   }
+
+  appointments:Appointment[] = [];
+
+
+  getAllAppointments(): void {
+    this.appointmentService.getAllAppointments().subscribe(
+      (data) => {this.appointments = data});
+
+  }
+
+
 
 }
---
+++ src/app/services/appointment.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Appointment } from '../models/appointment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getAllAppointments() : Observable<Appointment[]> {
+
+    return this.httpClient.get("http://localhost:8080/ePatient/appointment/all") as Observable<Appointment[]>;
+  }
+}
