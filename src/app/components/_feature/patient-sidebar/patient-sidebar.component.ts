2b26bf38767935577f0e2a70c4a4aaed9dd04783
mimel <imel.matt.r@gmail.com>
2020-11-05 23:09:00 -0600

    Added styles to patient pages.

---
+++ src/app/components/_feature/patient-sidebar/patient-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-sidebar',
+  templateUrl: './patient-sidebar.component.html',
+  styleUrls: ['./patient-sidebar.component.css']
+})
+export class PatientSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
