893111a59ed98a885483beb7e9182270a5327deb
mimel <imel.matt.r@gmail.com>
2020-11-07 14:43:23 -0600

    Added response bubbles. Fixed form submission issues that could be patch with session data.

--- src/app/components/patient-info/patient-info.component.ts
+++ src/app/components/patient-info/patient-info.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Person } from 'src/app/models/person';
 import { GetPatientService } from 'src/app/services/get-patient.service';
+import { ResponseBubbleService } from 'src/app/services/response-bubble.service';
+import { ResponseBubble } from 'src/app/ui/response_bubble';
 
 @Component({
   selector: 'app-patient-info',
@@ -9,12 +11,17 @@ import { GetPatientService } from 'src/app/services/get-patient.service';
   styleUrls: ['./patient-info.component.css']
 })
 export class PatientInfoComponent implements OnInit {
+  responseBubble: ResponseBubble;
+
   personId: number;
   person: Person = new Person(0, "", "", "", null);
 
-  constructor(private personService: GetPatientService, private route: ActivatedRoute) { }
+  constructor(private personService: GetPatientService, private responseBubbleService: ResponseBubbleService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.responseBubble = this.responseBubbleService.getBubble();
+    this.responseBubbleService.setBubble(null);
+    
     this.personId = parseInt(this.route.snapshot.paramMap.get("id"));
     this.getPersonInfo(this.personId);
   }
