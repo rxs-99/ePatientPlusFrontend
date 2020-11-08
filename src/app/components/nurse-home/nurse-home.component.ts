28140336c6db98a5ec22ad015b00f030e0f0e214
zach scharoun <zscharoun@gmail.com>
2020-11-07 22:49:27 -0500

    Nurse page is 99% complete need to test profile page

--- src/app/components/nurse-home/nurse-home.component.ts
+++ src/app/components/nurse-home/nurse-home.component.ts
@@ -5,7 +5,11 @@ import { Appointment } from 'src/app/models/appointment';
 import { Person } from 'src/app/models/person';
 import { AppointmentService } from 'src/app/services/appointment.service';
 import {GetPatientService} from 'src/app/services/get-patient.service'
-
+import { ResponseBubble } from 'src/app/ui/response_bubble';
+import { ResponseBubbleService } from 'src/app/services/response-bubble.service';
+import * as EmailValidator from 'email-validator';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-nurse-home',
   templateUrl: './nurse-home.component.html',
@@ -13,11 +17,38 @@ import {GetPatientService} from 'src/app/services/get-patient.service'
 })
 export class NurseHomeComponent implements OnInit {
 
-  constructor(private appointmentService:AppointmentService, private patientService:GetPatientService) { }
+  constructor(private appointmentService:AppointmentService, private patientService:GetPatientService, private responseBubbleService: ResponseBubbleService, 
+    private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.getAllAppointments()
     this.getAllPatients()
+
+    // code below is from patient-profile-edit
+
+    this.responseBubble = this.responseBubbleService.getBubble();
+    this.responseBubbleService.setBubble(null);
+
+    this.personId = parseInt(this.route.snapshot.paramMap.get("id"));
+    this.personEditForm = this.formBuilder.group({
+      "name": "",
+      "email": "",
+      "phone": ""
+    });
+
+    this.patientService.getPerson(this.personId).subscribe(
+      (person) => {
+        this.person = person;
+        this.personEditForm.setValue({
+          "name": person.name,
+          "email": person.email,
+          "phone": person.phone
+        })
+      },
+      () => {
+        console.log("An error has occurred when retrieving Person info.");
+      }
+    );
   }
 
   appointments:Appointment[] = [];
@@ -31,12 +62,14 @@ export class NurseHomeComponent implements OnInit {
   showModal:boolean = false;
 
 
+
   showAllAppointments:boolean = false;
   showPending:boolean = true;
   showPatients:boolean = false;
   showCancelApproved:boolean = false;
+  showProfile:boolean = false;
 
-  showMustHaveReason:boolean = false
+  showMustHaveReason:boolean = false;
   
   chosen:number = -1;
   reason:string;
@@ -44,6 +77,11 @@ export class NurseHomeComponent implements OnInit {
 
   toggleModal(index: number)
   {
+    if(this.showMustHaveReason)
+    {
+      this.showMustHaveReason = false
+    }
+
     this.showModal = !this.showModal;
     this.chosen = index;
   }
@@ -73,6 +111,7 @@ export class NurseHomeComponent implements OnInit {
     this.showPending = choice === 'pending'
     this.showPatients = choice === 'patients'
     this.showCancelApproved = choice === 'approved'
+    this.showProfile = choice === 'profile'
   }
 
 
@@ -174,6 +213,56 @@ export class NurseHomeComponent implements OnInit {
         this.allPatients = data;
       });
 
+  }
+
+//Code below is from patient-profile-edit.component
+
+  responseBubble: ResponseBubble;
+
+  personId: number;
+  person: Person = new Person(0, "", "", "", null);
+  personEditForm: FormGroup;
+
+  
+  editPersonInfo(personData: any): void {
+    // Validation.
+    // Regex created by Don Johnston.
+	  // https://regexlib.com/REDetails.aspx?regexp_id=607
+    let phoneRegex: RegExp = /^(?:\([2-9]\d{2}\)\ ?|[2-9]\d{2}(?:\-?|\ ?))[2-9]\d{2}[- ]?\d{4}$/
+
+    if(!personData.name) {
+      this.responseBubble = new ResponseBubble(true, "Please enter a valid name.");
+      return;
+    } else if(!EmailValidator.validate(personData.email)) {
+      console.dir(personData.email);
+      this.responseBubble = new ResponseBubble(true, "Please enter a valid email address.");
+      return;
+    } else if(!phoneRegex.test(personData.phone)) {
+      this.responseBubble = new ResponseBubble(true, "Please enter a valid phone.");
+      return;
+
+
+    }
+
+    this.person.name = personData.name;
+    this.person.email = personData.email;
+    this.person.phone = personData.phone;
+    
+    this.patientService.setPerson(this.personId, this.person).subscribe(
+      () => {
+       // this.router.navigate(['/profile/' + this.personId])
+
+        this.responseBubbleService.setBubble(new ResponseBubble(false, "Your profile was successfully changed."));
+      },
+      () => {
+        console.log("An error has occurred when retrieving Person info.");
+      }
+    )
+  }
+
+  clearResponseBubble() {
+    this.responseBubbleService.setBubble(null);
+    this.responseBubble = null;
   }
 
 }
